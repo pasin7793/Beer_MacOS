7358af66d0e25671f4dcce03bc9b28eb1f2ed669
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-11-02 23:14:27 +0900

    Initial Commit

---
+++ Beer_MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Beer_MacOS
+//
+//  Created by 임준화 on 2022/11/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
