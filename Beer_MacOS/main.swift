2e80bcb3640b396891c8d64c15469b55d9435433
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-11-02 23:17:20 +0900

    main

---
+++ Beer_MacOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Beer_MacOS
+//
+//  Created by 임준화 on 2022/11/02.
+//
+
+import AppKit
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
